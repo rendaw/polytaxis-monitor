da52fa13a08472a5ad6aa0c2d9c0cf0cde36309c
Rendaw <rendaw@zarbosoft.com>
2015-06-21 01:14:10 +0900

    Updated setup

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
 setup(
-    name = 'polytaxis-utils',
+    name = 'polytaxis-monitor',
     version = '0.0.1',
     author = 'Rendaw',
     author_email = 'spoo@zarbosoft.com',
-    url = 'https://github.com/Rendaw/ptutils',
-    download_url = 'https://github.com/Rendaw/ptutils/tarball/v0.0.1',
+    url = 'https://github.com/Rendaw/polytaxis-monitor',
+    download_url = 'https://github.com/Rendaw/polytaxis-monitor/tarball/v0.0.1',
     license = 'BSD',
-    description = 'Utilities for working with polytaxis files.',
+    description = 'Indexes polytaxis tags for lookup by various programs',
     long_description = open('readme.md', 'r').read(),
     classifiers = [
         'Development Status :: 3 - Alpha',
@@ -19,24 +19,15 @@ setup(
         'appdirs',
         'watchdog',
         'natsort',
-        'python-magic',
-        'ExifRead',
-        'pytaglib',
     ],
     packages = [
         'polytaxis_monitor', 
         'ptq', 
-        'polytaxis_import',
-        'polytaxis_cleanup',
-        'unpt',
     ],
     entry_points = {
         'console_scripts': [
             'polytaxis-monitor = polytaxis_monitor.main:main',
             'ptq = ptq.main:main',
-            'polytaxis-import = polytaxis_import.main:main',
-            'polytaxis-cleanup = polytaxis_cleanup.main:main',
-            'unpt = unpt.main:main',
         ],
     },
 )
